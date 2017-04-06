a1a345cf81ac3b9361553d9e3979ce32ff54b7a8
Brady Hammond <brady@Bradys-MacBook-Pro-2.local>
2017-04-06 13:51:01 -0600

    More Progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.general')
+
+@section('title', 'Home')
 @section('stylesheets')
     <link rel="stylesheet" type="text/css" href="css/app.css"> 
     <link rel="stylesheet" type="text/css" href="css/main.css">
--- resources/views/lagerlöf_word.blade.php
+++ resources/views/lagerlöf_word.blade.php
@@ -12,7 +12,7 @@
 	<div class="jumbotron jumbotron-default-background after_navbar">
         <div class="container text-center">
             <h1>
-            	Words Containing {{mb_convert_case($word, MB_CASE_TITLE, "UTF-8")}}
+            	Topics Containing {{mb_convert_case($word, MB_CASE_TITLE, "UTF-8")}}
             </h1>
 	    	<form class="form-center" id="searchbox" autocomplete="off" action="/projects/1/Selma%20Lagerlöf%20Project/word_comparison/{{$word}}" method="post">
 	            <input name="search" type="text" size="15" placeholder="search words"/>
@@ -20,80 +20,90 @@
 	        </form>
         </div>
     </div>
-    <table class="table">
-        <tbody>
-    	    @php
-    			$i = 0;
-    		@endphp
-        	@foreach($topics as $topic)
-	            <tr>
-	                <td class="text-center col-md-4 col-sm-4 col-xs-4 align-middle">
-	                	<h4>
-		                	<a class="gray-link" href="/projects/1/Selma%20Lagerlöf%20Project/{{str_replace('/', '-', $topic->global_id)}}">
-	                            {{substr($main->where('global_id', $topic->global_id)->first()->chunk_size, 0, 4) . ' Word Chunks'}}
-	                            @if (strpos($main->where('global_id', $topic->global_id)->first()->chunk_size, 'NA'))
-	                                {{'(Nouns and Adjectives), '}}
-	                            @else
-	                                {{'(Nouns), '}}
-	                            @endif
-	                            {{$main->where('global_id', $topic->global_id)->first()->topic_id . '/' . $main->where('global_id', $topic->global_id)->first()->topic_number}}
-	                        </a>
-	                    </h4>
-	                    <div>
-		                    @if($main->where('global_id', $topic->global_id)->first()->topic_name != " " && $main->where('global_id', $topic->global_id)->first()->topic_name != null)
-		                        {{ $main->where('global_id', $topic->global_id)->first()->topic_name }}
-		                    @else
-		                        To Be Determined
-		                    @endif
-		                </div>
-	                </td>
-	                <td>
-	                	@php
-	                		$weight_list = $words[$i]->pluck('weight');
-                            $weight_size = count($weight_list);
-                            $data_weights = "";
+    @if(!$topics->isEmpty())
+	    <table class="table">
+	        <tbody>
+	    	    @php
+	    			$i = 0;
+	    		@endphp
+	        	@foreach($topics as $topic)
+		            <tr>
+		                <td class="text-center col-md-4 col-sm-4 col-xs-4 align-middle">
+		                	<h4>
+			                	<a class="gray-link" href="/projects/1/Selma%20Lagerlöf%20Project/{{str_replace('/', '-', $topic->global_id)}}">
+		                            {{substr($main->where('global_id', $topic->global_id)->first()->chunk_size, 0, 4) . ' Word Chunks'}}
+		                            @if (strpos($main->where('global_id', $topic->global_id)->first()->chunk_size, 'NA'))
+		                                {{'(Nouns and Adjectives), '}}
+		                            @else
+		                                {{'(Nouns), '}}
+		                            @endif
+		                            {{$main->where('global_id', $topic->global_id)->first()->topic_id . '/' . $main->where('global_id', $topic->global_id)->first()->topic_number}}
+		                        </a>
+		                    </h4>
+		                    <div>
+			                    @if($main->where('global_id', $topic->global_id)->first()->topic_name != " " && $main->where('global_id', $topic->global_id)->first()->topic_name != null)
+			                        {{ $main->where('global_id', $topic->global_id)->first()->topic_name }}
+			                    @else
+			                        To Be Determined
+			                    @endif
+			                </div>
+		                </td>
+		                <td>
+		                	@php
+		                		$weight_list = $words[$i]->pluck('weight');
+	                            $weight_size = count($weight_list);
+	                            $data_weights = "";
 
-                            for($j=0; $j < $weight_size; $j++)
-                            {
-		                		if($j == ($weight_size - 1))
-		                		{
-		                			$data_weights = $data_weights . $weight_list[$j];
+	                            for($j=0; $j < $weight_size; $j++)
+	                            {
+			                		if($j == ($weight_size - 1))
+			                		{
+			                			$data_weights = $data_weights . $weight_list[$j];
+			                		}
+
+			                		else
+			                		{
+			                			$data_weights = $data_weights . $weight_list[$j] . ",";
+			                		}
 		                		}
 
-		                		else
-		                		{
-		                			$data_weights = $data_weights . $weight_list[$j] . ",";
-		                		}
-	                		}
+		                		$word_list = $words[$i]->pluck('word');
+		                		$word_size = count($word_list);
+		                		$data_words = "";
 
-	                		$word_list = $words[$i]->pluck('word');
-	                		$word_size = count($word_list);
-	                		$data_words = "";
-
-	                		foreach($word_list as $topic_word)
-	                		{
-								if($word_list[$word_size -1] != $topic_word)
+		                		foreach($word_list as $topic_word)
 		                		{
-		                			$data_words = $data_words . $topic_word . ",";
-		                		}
+									if($word_list[$word_size -1] != $topic_word)
+			                		{
+			                			$data_words = $data_words . $topic_word . ",";
+			                		}
 
-		                		else
-		                		{
-		                			$data_words = $data_words . $topic_word;
+			                		else
+			                		{
+			                			$data_words = $data_words . $topic_word;
+			                		}
 		                		}
-	                		}
-	                	@endphp
-	                	<svg class="bar-graph" data-bar-chart data-weights="{{$data_weights}}" data-words="{{$data_words}}" style="width: 100%">
-	                		
-	                	</svg>
-	                </td>
-	            </tr>
-	            @php
-	            	$i = $i + 1;
-	            @endphp
-            @endforeach
-        </tbody>
-    </table>
+		                	@endphp
+		                	<svg class="bar-graph" data-bar-chart data-weights="{{$data_weights}}" data-words="{{$data_words}}" style="width: 100%">
+		                		
+		                	</svg>
+		                </td>
+		            </tr>
+		            @php
+		            	$i = $i + 1;
+		            @endphp
+	            @endforeach
+	        </tbody>
+	    </table>
+	@else
+		<div class="jumbotron jumbotron-default-background after_navbar">
+	        <div class="container text-center">
+	            <h5>
+	            	No Results Found
+	            </h5>
+	        </div>
+	    </div>
+	@endif
 @endsection
 
 @section('scripts')
